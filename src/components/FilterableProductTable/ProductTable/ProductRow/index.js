ed81f9dd866df81bd9890f70987a5481f2d53556
Aidos <aidos_ka@yahoo.com>
2020-07-31 16:37:04 -0500

    Add Product Row

--- src/components/FilterableProductTable/ProductTable/ProductRow/index.js
+++ src/components/FilterableProductTable/ProductTable/ProductRow/index.js
@@ -1,3 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const ProductRow = () => <p>ProductCategoryRow</p>
+export const ProductRow = ({name,price}) => <tr><td>{name}</td><td>{price}</td></tr>
+
+
+ProductRow.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string
+}
--- src/components/FilterableProductTable/ProductTable/index.js
+++ src/components/FilterableProductTable/ProductTable/index.js
@@ -1,10 +1,30 @@
-import React, {Fragment} from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import { ProductCategoryRow } from "./ProductCategoryRow";
+import { ProductRow } from "./ProductRow";
 
-import { ProductCategoryRow } from "./ProductCategoryRow"
-import { ProductRow} from "./ProductRow"
+// ToDO: map over products and return a product row with name and price for each product
+export const ProductTable = ({ products }) => {
+  const renderProductRows = () => {
+    products.map(({ name, price }, i) =>  {
+      <ProductRow name={name} price={price} key={i} />
+    })};
 
-export const ProductTable = () => (<Fragment>
-    <p>ProductTable</p>
-    <ProductCategoryRow />
-    <ProductRow />
-  </Fragment>)
+  return (
+    <table>
+    <thead>
+    <tr>
+    <tbody>
+     <ProductCategoryRow />
+
+      {renderProductRows()}
+      </tbody>
+      </tr>
+      </thead>
+    </table>
+  );
+};
+
+ProductTable.propTypes = {
+  products: PropTypes.array.isRequired,
+};
