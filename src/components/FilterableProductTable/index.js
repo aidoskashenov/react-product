240c81b5dd66d5becad494fd97603752868e4b59
Aidos <aidos_ka@yahoo.com>
2020-07-31 14:57:28 -0500

    Add state and async-await

--- src/components/FilterableProductTable/index.js
+++ src/components/FilterableProductTable/index.js
@@ -1,14 +1,31 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-import {ProductTable} from "./ProductTable"
-import {SearchBar} from "./SearchBar"
+import { ProductTable } from "./ProductTable";
+import { SearchBar } from "./SearchBar";
 
-export const FilterableProductTable = () =>
-<Fragment>
-<p>Productable</p>
-  <ProductTable/>
-  <SearchBar/>
-</Fragment>
+export class FilterableProductTable extends React.Component {
+  state = {
+    products: [],
+    searchText: "",
+  };
+
+  async componentDidMount() {
+    const res = await fetch(
+      "https://my-json-server.typicode.com/Claim-Academy-JS/products/products"
+    );
 
 
 
+    this.setState({ products: await res.json()});
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <p>FilterableProductTable</p>
+        <ProductTable />
+        <SearchBar />
+      </Fragment>
+    );
+  }
+}
